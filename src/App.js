5385a922c724e19bc3661e96b1b7130b589d40db
yo <y.n_green@hotmail.co.jp>
2023-01-03 13:44:40 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialValues = { username: "", mailAddress: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formContainer">
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input
+              type="text"
+              placeholder="ユーザー名"
+              name="username"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input
+              type="text"
+              placeholder="メールアドレス"
+              name="mailAddress"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input
+              type="text"
+              placeholder="パスワード"
+              name="password"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+          <button className="submitButton">ログイン</button>
+        </div>
+      </form>
     </div>
   );
 }
